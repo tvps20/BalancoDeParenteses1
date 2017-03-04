2ade431da5febe81d4c86e83c7a0f0b6fab4b778
Thiago Santiago <t_vps20@gmail.com>
2017-03-04 11:44:35 -0300

    Implementado Metodo

--- BalancoDeParenteses/src/balancodeparenteses/BalancoDeParenteses.java
+++ BalancoDeParenteses/src/balancodeparenteses/BalancoDeParenteses.java
@@ -3,8 +3,26 @@ package balancodeparenteses;
 
 public class BalancoDeParenteses {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    public boolean TestarParenteses(String expressao)
+    {
+        char[] expressaoList = expressao.toCharArray(); // convertendo a expressão para uma lista de caracteres
+        
+        int parentesesAbertos = 0;
+        int parentesesFechados = 0;
+
+        for(int i=0; i<expressaoList.length; i++)
+        {
+            if(parentesesAbertos == 0 && expressaoList[i] == ')') //Se o primeiro parenteses é de fechamento
+                return false;
+            else if(expressaoList[i] == '(')
+                parentesesAbertos++;
+            else if(expressaoList[i] == ')')
+                parentesesFechados++;
+        }
+        
+        if(parentesesAbertos == parentesesFechados)
+            return true;
+        else
+            return false;     
+    } 
 }
