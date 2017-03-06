6366f87ad61c985bb984da4512583e1b598db455
Thiago Santiago <thiago.vps20@gmail.com>
2017-03-05 21:57:54 -0300

    utilizando loop

--- BalancoDeParenteses/src/balancodeparenteses/Teste.java
+++ BalancoDeParenteses/src/balancodeparenteses/Teste.java
@@ -1,21 +1,33 @@
 package balancodeparenteses;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Teste 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
-        BalancoDeParenteses novoBalanco = new BalancoDeParenteses();
-        
-        String teste1 = "a+(b*c)-2-a";
+    /*    String teste1 = "a+(b*c)-2-a ";
         String teste2 = "(a+b*(2-c)-2+a)*2";
         String teste3 = "(a*b-(2+c)";
         String teste4 = "2*(3-a))";
         String teste5 = ")3+b*(2-c)(";
+        String teste6 = "())(";
         
-        System.out.println(novoBalanco.TestarParenteses(teste1));
-        System.out.println(novoBalanco.TestarParenteses(teste2));
-        System.out.println(novoBalanco.TestarParenteses(teste3));
-        System.out.println(novoBalanco.TestarParenteses(teste4));
-        System.out.println(novoBalanco.TestarParenteses(teste5));
-    }  
+        System.out.println(TestarParenteses(teste1));
+        System.out.println(TestarParenteses(teste2));
+        System.out.println(TestarParenteses(teste3));
+        System.out.println(TestarParenteses(teste4));
+        System.out.println(TestarParenteses(teste5));
+        //TestarParenteses2(teste6);
+    */    
+        
+        Scanner leitor = new Scanner(System.in);
+        while(leitor.hasNext()) 
+        {
+            String expressao = leitor.next();
+            System.out.println(TestarParenteses(expressao));
+        }        
+    }
 }
